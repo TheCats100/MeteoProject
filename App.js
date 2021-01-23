b6a08c4579eca21526d97fd0031df9995eea2b1a
TheCats100 <m-g-152@outlook.fr>
2021-01-23 12:22:09 +0100

    feat(app): icon usable

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
 import React from 'react';
+import { View, StatusBar, StyleSheet, Image } from 'react-native';
 import About from './components/About';
 import Search from './components/Search';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
+const iconToAbout = () => <Image source={require('./components/icons/user.png')} style={{ width: 20, height: 20 }} />
+const iconToSearch = () => <Image source={require('./components/icons/accueil.png')} style={{ width: 20, height: 20 }} />
+
 const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Search" component={Search} />
-        <Tab.Screen name="About" component={About} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={{flex: 1}}>
+      <StatusBar hidden={true} />
+      <NavigationContainer>
+        <Tab.Navigator tabBarOptions={{
+          showIcon: true,
+          showLabel: false,
+          style: style.navBar,
+          indicatorStyle: style.indicator
+        }}>
+          <Tab.Screen 
+            name="Search" 
+            component={Search}
+            options={{ 
+              title: 'Search',
+              tabBarIcon: iconToSearch
+            }} 
+          />
+          <Tab.Screen 
+            name="About" 
+            component={About}
+            options={{ 
+              title: 'About',
+              tabBarIcon: iconToAbout
+            }} 
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
 
+const style = StyleSheet.create({
+  navBar: {
+    backgroundColor: "#a2273C",
+    borderTopWidth: 1,
+    borderColor: "#3f101C"
+  },
+  indicator: {
+    height: 2,
+    backgroundColor: "#FFF"
+  }
+})
