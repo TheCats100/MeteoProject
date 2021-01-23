36aa90d367883ede838834e501eb17200cffbc43
TheCats100 <m-g-152@outlook.fr>
2021-01-23 14:30:40 +0100

    faet(app): install axios

--- components/Results.js
+++ components/Results.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import { connect } from 'react-redux';
+import Axios from 'axios';
 
 const mapStateToProps = (state) => ({
   city: state.city
 })
 
 const Results = ({ city }) => {
+
+  const [weather, setWeather] = useState(null)
+
+  const fetchWeather = () => {
+    const url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metri&cnt=10&APPID=94c6cf0868fa5cb930a5e2d71baf0dbf`
+    Axios.get(url)
+      .then(res => res.data)
+      .then(data => setWeather(data))
+      .catch(err => console.log(err))
+  }
+
   return (
     <View>
       <Text>
         {city}
       </Text>
-      <Button title='Show me city' onPress={() => console.log(city)} />
+      <Button title='Show me city' onPress={() => fetchWeather()} />
     </View>
   )
 }
